8990ce01a7b690767b7d4c0d3c5164b720c466f3
Lukas <lukas.otero@davinci.edu.ar>
2022-04-23 10:42:58 -0300

    Arreglo del boton para las traducciones

--- assets/js/botonLang.js
+++ assets/js/botonLang.js
@@ -8,16 +8,23 @@ function cambiarEstado(period) {
         selector.style.left = 0;
         selector.style.width = esLang.clientWidth + "px";
         selector.style.backgroundColor = "#c99900";
-        selector.innerHTML = "esLang";
+        selector.innerHTML = '<span class="fi fi-es"></span>';
     } else if (period === "brLang") {
         selector.style.left = esLang.clientWidth + "px";
         selector.style.width = brLang.clientWidth + "px";
-        selector.innerHTML = "brLang";
+        selector.innerHTML = '<span class="fi fi-br"></span>';
         selector.style.backgroundColor = "#017c2f";
     } else {
         selector.style.left = esLang.clientWidth + brLang.clientWidth + 1 + "px";
         selector.style.width = usLang.clientWidth + "px";
-        selector.innerHTML = "usLang";
+        selector.innerHTML = '<span class="fi fi-us"></span>';
         selector.style.backgroundColor = "#2a3a69";
     }
 }
+
+/*
+COLORES:
+España -> background-color: #c99900; background-color: #8c0a0f;
+Brasil -> background-color: #017c2f; background-color: #cbb200; background-color: #011f5e;
+Estados Unidos -> background-color: #2a3a69; background-color: #9e2c45; background-color: #f2f2f2;
+*/
