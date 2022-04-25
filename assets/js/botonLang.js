468a2a93cbee086165e076f418cb4d28b8c186e2
Lukas <lukas.otero@davinci.edu.ar>
2022-04-25 16:19:57 -0300

    Arreglo en el Boton Lang

--- assets/js/botonLang.js
+++ assets/js/botonLang.js
@@ -1,20 +1,43 @@
 function cambiarEstado(period) {
-    var esLang = document.getElementById("esLang");
-    var brLang = document.getElementById("brLang");
-    var usLang = document.getElementById("usLang");
-    var selector = document.getElementById("selector");
+    let esLang = document.getElementById("esLang");
+    let brLang = document.getElementById("brLang");
+    let usLang = document.getElementById("usLang");
+    let selector = document.getElementById("selector");
 
     if (period === "esLang") {
-        selector.style.left = 0;
-        selector.style.width = esLang.clientWidth + "px";
-        selector.style.backgroundColor = "#c99900";
-        selector.innerHTML = '<span class="fi fi-es"></span>';
+
+        if (!esLang.classList.contains("active")) {
+            selector.style.left = 0;
+            selector.style.width = esLang.clientWidth + "px";
+    
+            selector.style.backgroundColor = "#c99900";
+            selector.innerHTML = '<span class="fi fi-es"></span>';
+        }
+
     } else if (period === "brLang") {
+
+        if (esLang.classList.contains("active")) {
+            esLang.classList.remove("active");
+
+            selector.style.left = 0;
+            selector.style.left = esLang.clientWidth + "px";
+            selector.style.width = brLang.clientWidth + "px";
+        }
+
         selector.style.left = esLang.clientWidth + "px";
         selector.style.width = brLang.clientWidth + "px";
         selector.innerHTML = '<span class="fi fi-br"></span>';
         selector.style.backgroundColor = "#017c2f";
+
     } else {
+        if (esLang.classList.contains("active")) {
+            esLang.classList.remove("active");
+
+            selector.style.left = 0;
+            selector.style.left = esLang.clientWidth + brLang.clientWidth + 1 + "px";
+            selector.style.width = usLang.clientWidth + "px";
+        }
+
         selector.style.left = esLang.clientWidth + brLang.clientWidth + 1 + "px";
         selector.style.width = usLang.clientWidth + "px";
         selector.innerHTML = '<span class="fi fi-us"></span>';
